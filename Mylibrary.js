373b1f4b125e7ca226a62490866e25a0e0f2e9bc
김지운 <expect.ta@gmail.com>
2021-09-05 23:47:14 +0900

    auto commit

--- Mylibrary.js
+++ Mylibrary.js
@@ -1,1 +1,18 @@
-//첫번째 라이브러리
+//문자열 합치기
+#include <iostream>
+#include <string>
+
+using std::cout; using std::endl;
+using std::copy; using std::string;
+
+int main() {
+    string string1("Starting string ");
+    string string2("end of the string ");
+
+    cout << "string1:  " << string1 << endl;
+    string1 += string2;
+    cout << "string1:  " << string1 << endl;
+
+    return EXIT_SUCCESS;
+}
+//두번째 코드
