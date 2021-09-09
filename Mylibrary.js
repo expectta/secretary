1b34f784a57943bb4bf39c07537a517daa58e943
김지운 <expect.ta@gmail.com>
2021-09-09 19:27:05 +0900

    auto commit

--- Mylibrary.js
+++ Mylibrary.js
@@ -31,3 +31,28 @@ const options = {
 const specs = swaggerJsdoc(options);
 module.exports = { swaggerUi, specs };
 
+//nest.js swagger 설정 파일로
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+/**
+ * Swagger 세팅
+ *
+ * @param {INestApplication} app
+ */
+export function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('datda API Document')
+    .setDescription('datda service API 문서입니다.')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api_docs', app, document);
+}
+
+//typeorm으로 데이터 베이스 내 entity 모두 가져오는 방법
+//npm i -g typeorm-model-generator 설치
+//typeorm-model-generator -h <database_host> -d <database>-p <port> -u <userId> -x <userPassword>! -e <database종류>-o <저장위치> 
+//다운로드가 완료되면 entities라는 파일로 저장 된다.
+
